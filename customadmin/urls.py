08232907737204958aa72dc816fc08cbf3ea1452
mdashraful <mdashrafulislam369@gmail.com>
2025-03-23 11:49:08 +0600

    forgote pasword

--- customadmin/urls.py
+++ customadmin/urls.py
@@ -11,4 +11,10 @@ urlpatterns = [
     path('users/create/', views.create_user, name='create_user'),
     path('users/edit/<int:pk>/', views.edit_user, name='edit_user'),
     path('users/delete-user/<int:pk>/', views.delete_user, name='delete_user'),
+
+
+    path("password_reset/", auth_views.PasswordResetView.as_view(), name="password_reset"),
+    path("password_reset/done/", auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path("reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path("reset/done/", auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
 ]
