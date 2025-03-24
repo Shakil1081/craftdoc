5ca666b80ce1ae204a6ea74eee242f302c2d5e94
tariqul-babylon <tariqul@newgen-bd.com>
2025-03-24 09:57:12 +0600

    update

--- customadmin/urls.py
+++ customadmin/urls.py
@@ -9,14 +9,13 @@ from . import auth_group_views
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='customadmin/login.html'), name='login'),  # Custom login
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
-    path('', custom_admin_dashboard, name='custom_admin_dashboard'),  # Custom dashboard
+    path('', custom_admin_dashboard, name='custom_admin_dashboard'),  
     path('users/', views.users_list, name='users_list'),
     path('users/create/', views.create_user, name='create_user'),
     path('users/edit/<int:pk>/', views.edit_user, name='edit_user'),
     path('users/delete-user/<int:pk>/', views.delete_user, name='delete_user'),
 
 
-<<<<<<< HEAD
 
     path("permission/", permission_views.permission_list, name="permission_list"),
     path("permission/create/", permission_views.permission_create, name="permission_create"),
@@ -37,10 +36,8 @@ urlpatterns = [
     path('group/delete/<int:group_id>/', auth_group_views.group_delete, name='group_delete'),
 
     
-=======
     path("password_reset/", auth_views.PasswordResetView.as_view(), name="password_reset"),
     path("password_reset/done/", auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
     path("reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
     path("reset/done/", auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
->>>>>>> 08232907737204958aa72dc816fc08cbf3ea1452
 ]
