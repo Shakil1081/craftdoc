ee104acadf3a4111759572b6af33583b5c776226
maruf-229 <mhmaruf229@gmail.com>
2025-05-11 17:37:29 +0600

    pdf generate

--- customadmin/models.py
+++ customadmin/models.py
@@ -210,7 +210,7 @@ class CreditUsesHistory(models.Model):
     target_id = models.IntegerField()
     description = models.TextField()
     usage_credit = models.DecimalField(max_digits=10, decimal_places=2)
-    created_at = models.DateTimeField(default=datetime.now)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.user.username} used {self.usage_credit}"
@@ -259,7 +259,7 @@ class DownloadHistory(models.Model):
     footer_path = models.CharField(max_length=255, blank=True)
     download_type = models.CharField(max_length=10)  # 'pdf', 'jpg', 'png'
     letterhead_content = models.TextField(blank=True, null=True)
-    created_at = models.DateTimeField(default=datetime.now)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.user} ({self.created_at})"
