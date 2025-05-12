2b5b7df26daf85174c142bd0f74d88b10ba8f030
tariqul-babylon <tariqul@newgen-bd.com>
2025-05-12 16:09:22 +0600

    update

--- adminlte2/settings.py
+++ adminlte2/settings.py
@@ -75,8 +75,8 @@ WSGI_APPLICATION = 'adminlte2.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',  # Use PostgreSQL engine
-        'NAME': 'postgres',  # Replace with your PostgreSQL database name
-        'USER': 'postgres',  # PostgreSQL user (typically 'postgres')
+        'NAME': 'craft_doc',  # Replace with your PostgreSQL database name
+        'USER': 'craft_doc',  # PostgreSQL user (typically 'postgres')
         'PASSWORD': 'Pga@2022',  # Replace with your PostgreSQL password
         'HOST': '127.0.0.1',  # Localhost for PostgreSQL
         'PORT': '5432',  # Default PostgreSQL port
